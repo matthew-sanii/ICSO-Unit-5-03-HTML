3ecb028e8fbd67593a8538fdc1e41a15474e3ced
matthew.sanii <matthew.sanii@mths.ca>
2020-05-12 20:26:19 +0000

    edited index.html, click.js, and text.css

--- click.js
+++ click.js
@@ -4,13 +4,16 @@ function Click() {
   let answer=document.getElementById("answer");
   let answerValue;
   answerValue=Number(answer.value); // This turns the number you guess from a string to a number
-  if (answerValue<0) { // This if statement checks to see if the diameter is less than 0, or had a letter in it when the button was clicked
-    response.innerHTML="This number is negative.";
+  if (answerValue>=17) { // This if statement checks to see if the diameter is less than 0, or had a letter in it when the button was clicked
+    response.innerHTML="You can see an R-rated movie alone";
   }
-  else if (answerValue==0){
-    response.innerHTML="The number is 0.";
+  else if (answerValue>=13){
+    response.innerHTML="You can watch a PG-13 movies alone";
+  }
+  else if (answerValue>=5){
+    response.innerHTML="G or PG movie alone.";
   }
   else {
-    response.innerHTML="This number is positive.";
+    response.innerHTML="Your too young for most movies.";
   }
 }
