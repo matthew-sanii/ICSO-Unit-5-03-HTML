6e468913b25f647429f7954fa1263e736c5f8c49
matthew.sanii <matthew.sanii@mths.ca>
2020-05-12 20:18:46 +0000

    edited index.html, click.js, and text.css

--- click.js
+++ click.js
@@ -1,1 +1,16 @@
 // JavaScript File
+let response=document.getElementById("response");
+function Click() {
+  let answer=document.getElementById("answer");
+  let answerValue;
+  answerValue=Number(answer.value); // This turns the number you guess from a string to a number
+  if (answerValue<0) { // This if statement checks to see if the diameter is less than 0, or had a letter in it when the button was clicked
+    response.innerHTML="This number is negative.";
+  }
+  else if (answerValue==0){
+    response.innerHTML="The number is 0.";
+  }
+  else {
+    response.innerHTML="This number is positive.";
+  }
+}
